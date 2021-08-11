90b5d8b60ea52d3b2dfae4791e77fe88e0ed7be9
Xelyfer <Xelyfer@gmail.com>
2021-08-12 01:17:23 +1200

    Added EJS and made my first EJS Template

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+      break;
+  }
+
+  res.render("list", { kindOfDay: day });
+});
+
+app.listen(port);
